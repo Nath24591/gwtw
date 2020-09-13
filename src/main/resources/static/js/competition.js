8df78c5d5da4a299dc6bf587586b9c605149e9ae
nathan <nathan@feefo.com>
2020-09-13 21:27:13 +0100

    Removed loading gif and ensures user is logged in to select tickets

--- src/main/resources/static/js/competition.js
+++ src/main/resources/static/js/competition.js
@@ -139,6 +139,10 @@ $(".answerClick").on('click', function() {
 $('.select-num').click( function() {
 
     //TODO Ensure user is logged in before they select tickets
+    if($("#loggedIn").length == 0){
+        basicSwal("error", "Please log in or create an account before selecting tickets.");
+        return false;
+    }
 
     //Set to active/not active on click
     if($($(this).parent().parent()[0]).hasClass("disabled")){
